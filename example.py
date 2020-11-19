ca2c5eb093368b133b222fd37a1fb26d0104a808
Sam Nooij <samnooij@protonmail.com>
2020-11-19 09:52:44 +0100

    add squuare function

--- example.py
+++ example.py
@@ -1,7 +1,12 @@
 def add(a, b):
-    return a - b
+    return a + b
 
 def test_add():
     assert add(2, 3) == 5
     assert add("hello", " world") == "hello world"
 
+def sqrt(x):
+    return x**(1./2)
+
+def test_sqrt():
+    assert round(sqrt(3.0)**2) == 3
